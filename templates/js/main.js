2a51efa52d58fee5d8a74d496ea076d636868f08
Neil Renicker <neilrenicker@gmail.com>
2013-11-03 21:11:58 -0500

    feat: begin js to include tumblr posts

---
+++ templates/js/main.js
@@ -0,0 +1,8 @@
+$.get('http://neilrenicker.tumblr.com/rss', function(data) {
+  posts = $(data).find("item");
+  randPost = $(posts[Math.floor(Math.random()*posts.length)]);
+  postContent = randPost.find("description").text();
+  postPermalink = randPost.find("link").text();
+
+  $(".js-addQuote").text(postContent);
+});
